8522f95addd86876462f0cdd9f9854c277b50170
ahmed osman <ahmedmido19782@gmail.com>
2024-07-19 17:01:19 +0300

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist' // Ensure the output directory is set to 'dist'
-  }
 })
