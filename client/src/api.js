f696d8cf58b9f022f8abb79297b9f81f3e1b6894
ahmed osman <ahmedmido19782@gmail.com>
2024-07-20 16:25:11 +0300

    fix

--- client/src/api.js
+++ client/src/api.js
@@ -4,21 +4,21 @@ import axios from 'axios';
 
 export const getActiveGame = async (puuid) => {
   const response = await axios.get(
-    `https://old-mate-server.vercel.app/api/active-game/${puuid}`
+    `http://localhost:3001/api/active-game/${puuid}`
   );
   return response.data;
 };
 
 export const getAccount = async (playerName, tag) => {
   const response = await axios.get(
-    `https://old-mate-server.vercel.app/api/account/${playerName}/${tag}`
+    `http://localhost:3001/api/account/${playerName}/${tag}`
   );
   return response.data;
 };
 
 export const getMatchHistory = async (puuid) => {
   const response = await axios.get(
-    `https://old-mate-server.vercel.app/api/match-history/${puuid}`
+    `http://localhost:3001/api/match-history/${puuid}`
   );
   console.log(response);
   return response.data;
@@ -26,14 +26,14 @@ export const getMatchHistory = async (puuid) => {
 
 export const getMatch = async (matchId) => {
   const response = await axios.get(
-    `https://old-mate-server.vercel.app/api/match/${matchId}`
+    `http://localhost:3001/api/match/${matchId}`
   );
   return response.data;
 };
 
 export const getSummoner = async (puuid) => {
   const response = await axios.get(
-    `https://old-mate-server.vercel.app/api/summoner/${puuid}`
+    `http://localhost:3001/api/summoner/${puuid}`
   );
   return response.data;
 };
--- server/app.js
+++ server/app.js
@@ -1,10 +1,104 @@
 const express = require("express");
 const app = express();
+const axios = require("axios");
+const path = require("path");
 
-app.use("/", (req, res) => {
-  res.send("Proxy server running...");
+require("dotenv").config();
+
+const cors = require("cors");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+
+const PORT = 3001;
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 100,
+  })
+);
+
+app.use(helmet());
+app.use(xss());
+app.use(cors());
+
+const apiKey = process.env.RIOT_KEY;
+
+app.get("/api/account/:playerName/:tag", async (req, res) => {
+  const { playerName, tag } = req.params;
+
+  try {
+    const response = await axios.get(
+      `https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${playerName}/${tag}`,
+      { headers: { "X-Riot-Token": apiKey } }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response.status).json({ message: error.message });
+  }
 });
 
-app.listen(5000, () => {
-  console.log(`Proxy server running on 5000 ${5000}`);
+app.get("/api/active-game/:puuid", async (req, res) => {
+  const { puuid } = req.params;
+
+  try {
+    const response = await axios.get(
+      `https://eun1.api.riotgames.com/lol/spectator/v5/active-games/by-summoner/${puuid}`,
+      {
+        headers: { "X-Riot-Token": apiKey },
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response.status).json({ message: error.message });
+  }
+});
+
+app.get("/api/match-history/:puuid", async (req, res) => {
+  const { puuid } = req.params;
+
+  try {
+    const response = await axios.get(
+      `https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=30`,
+      { headers: { "X-Riot-Token": apiKey } }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response.status).json({ message: error.message });
+  }
+});
+
+app.get("/api/match/:matchId", async (req, res) => {
+  const { matchId } = req.params;
+
+  try {
+    const response = await axios.get(
+      `https://europe.api.riotgames.com/lol/match/v5/matches/${matchId}`,
+      { headers: { "X-Riot-Token": apiKey } }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response.status).json({ message: error.message });
+  }
+});
+
+app.get("/api/summoner/:puuid", async (req, res) => {
+  const { puuid } = req.params;
+
+  try {
+    const response = await axios.get(
+      `https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}`,
+      { headers: { "X-Riot-Token": apiKey } }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response.status).json({ message: error.message });
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Proxy server running on port ${PORT}`);
 });
